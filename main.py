f7fff08628a553ad00c97de828aea8791716d630
Khasir <khasir.hean@gmail.com>
2022-12-16 01:01:43 -0500

    bugfixes

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 from bs4 import BeautifulSoup
 
+from crawl import TTCMeetingsChecker
+
 
 # filename = 'samples/Public meetings - true.html'
 # with open(filename) as file:
-# 	soup = BeautifulSoup(file.read(), 'html.parser')
+#   soup = BeautifulSoup(file.read(), 'html.parser')
 # tags = soup.find('ul', class_='search-result-list')
 # print(tags)
 
 def main():
-	checker = TTCMeetingsChecker('https://www.ttc.ca/public-meetings')
-	found = checker.check_upcoming_ttc_meetings()
-	return found
+    checker = TTCMeetingsChecker('https://www.ttc.ca/public-meetings')
+    found = checker.check_upcoming_ttc_meetings()
+    return found
 
 
 if __name__ == '__main__':
-	main()
+    print(main())
