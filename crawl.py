8939c49eb0f1778f3d9c3cd560248e3db9ecd7b5
Khasir <khasir.hean@gmail.com>
2022-12-16 01:24:29 -0500

    use session to store cookies

--- crawl.py
+++ crawl.py
@@ -6,9 +6,10 @@ class TTCMeetingsChecker:
     def __init__(self, upcoming_url: str, past_url: str):
         self.upcoming_url = upcoming_url
         self.past_url = past_url
+        self.session = requests.Session()
 
     def check_upcoming_ttc_meetings(self):
-        response = requests.get(self.upcoming_url)
+        response = self.session.get(self.upcoming_url)
         try:
             response.raise_for_status()
         except requests.exceptions.HTTPError as err:
