807a3b10499d5c733f68015eb31cb08fa6fc23f1
Khasir <khasir.hean@gmail.com>
2022-12-16 01:19:21 -0500

    use specific URLs

--- crawl.py
+++ crawl.py
@@ -3,11 +3,12 @@ from bs4 import BeautifulSoup
 
 
 class TTCMeetingsChecker:
-    def __init__(self, root_url: str):
-        self.root_url = root_url
+    def __init__(self, upcoming_url: str, past_url: str):
+        self.upcoming_url = upcoming_url
+        self.past_url = past_url
 
     def check_upcoming_ttc_meetings(self, suffix="?tab=0#upcoming_e=0&past_CustomDateRangeFacet=Past&upcoming_CustomDateRangeFacet=Upcoming"):
-        response = requests.get(self.root_url + suffix)
+        response = requests.get(self.upcoming_url)
         try:
             response.raise_for_status()
         except requests.exceptions.HTTPError as err:
--- main.py
+++ main.py
@@ -10,7 +10,10 @@ from crawl import TTCMeetingsChecker
 # print(tags)
 
 def main():
-    checker = TTCMeetingsChecker('https://www.ttc.ca/public-meetings')
+    checker = TTCMeetingsChecker(
+        upcoming_url='https://www.ttc.ca//sxa/search/results/?s={C3364F33-3058-498E-90F3-1B6D73D99718}&itemid={1450DB42-0543-4C73-B159-421DF22D9460}&sig=upcoming&customdaterangefacet=Upcoming&e=0&p=8&o=ContentDateFacet%2CAscending&v=%7BF9A088B4-AFC4-4EE7-8649-0ACA83AB2783%7D',
+        past_url='https://www.ttc.ca//sxa/search/results/?s={5865C996-6A4C-472A-9116-C59CB3B76093}&itemid={1450DB42-0543-4C73-B159-421DF22D9460}&sig=past&p=8&o=ContentDateFacet%2CDescending&v=%7BF9A088B4-AFC4-4EE7-8649-0ACA83AB2783%7D'
+    )
     found = checker.check_upcoming_ttc_meetings()
     return found
 
