15f2525dba80102ec7334e743ac1eb2b0d1a6e7c
Khasir <khasir.hean@gmail.com>
2022-12-16 01:22:11 -0500

    simplify upcoming url

--- crawl.py
+++ crawl.py
@@ -7,7 +7,7 @@ class TTCMeetingsChecker:
         self.upcoming_url = upcoming_url
         self.past_url = past_url
 
-    def check_upcoming_ttc_meetings(self, suffix="?tab=0#upcoming_e=0&past_CustomDateRangeFacet=Past&upcoming_CustomDateRangeFacet=Upcoming"):
+    def check_upcoming_ttc_meetings(self):
         response = requests.get(self.upcoming_url)
         try:
             response.raise_for_status()
