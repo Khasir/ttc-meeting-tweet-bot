292389c7a81ab24bcf65579a7381bb24f626b028
Khasir <khasir.hean@gmail.com>
2023-01-11 00:35:45 -0500

    trying out oauth refreshing

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import logging
 import os
 import re
 
+import oauthlib
 import redis
 import requests
 from bs4 import BeautifulSoup
@@ -30,7 +31,7 @@ client_secret = os.environ['CLIENT_SECRET']
 auth_url = "https://twitter.com/i/oauth2/authorize"
 token_url = "https://api.twitter.com/2/oauth2/token"
 redirect_uri = os.environ["REDIRECT_URI"]
-scopes = ["tweet.read", "users.read", "tweet.write", "offline.access"]
+scopes = ["tweet.write", "offline.access"]
 # Code verification
 code_verifier = base64.urlsafe_b64encode(os.urandom(30)).decode("utf-8")
 code_verifier = re.sub("[^a-zA-Z0-9]+", "", code_verifier)
@@ -47,28 +48,6 @@ log = logging.getLogger(__name__)
 
 def make_token():
     return OAuth2Session(client_id, redirect_uri=redirect_uri, scope=scopes)
-
-
-def check_upcoming():
-    checker = TTCMeetingsChecker(
-        upcoming_url='https://www.ttc.ca//sxa/search/results/?customdaterangefacet=Upcoming',
-        past_url='https://www.ttc.ca//sxa/search/results/?s={5865C996-6A4C-472A-9116-C59CB3B76093}&itemid={1450DB42-0543-4C73-B159-421DF22D9460}&sig=past&p=8&o=ContentDateFacet%2CDescending&v=%7BF9A088B4-AFC4-4EE7-8649-0ACA83AB2783%7D'
-    )
-    found = checker.check_upcoming_ttc_meetings()
-    return found
-
-
-def post_tweet(payload, token):
-    print("Tweeting!")
-    log.info("Tweeting!")
-    return requests.post(
-        "https://api.twitter.com/2/tweets",
-        json=payload,
-        headers={
-            "Authorization": "Bearer {}".format(token["access_token"]),
-            "Content-Type": "application/json",
-        },
-    )
 
 
 @app.route("/")
@@ -100,10 +79,97 @@ def callback():
     st_token = '"{}"'.format(token)
     j_token = json.loads(st_token)
     db.set("token", j_token)
-    msg = check_upcoming()
-    payload = {"text": "{}".format(msg)}
-    response = post_tweet(payload, token).json()
+    # msg = check_upcoming()
+    # payload = {"text": "Hello, world!"}
+    # response = post_tweet(payload, token).json()
+    response = json.dumps({'success': True})
     return response
+
+
+def post_tweet(payload, token):
+    log.info("Tweeting!")
+    return twitter.post(
+        "https://api.twitter.com/2/tweets",
+        json=payload,
+        headers={
+            "Authorization": "Bearer {}".format(token["access_token"]),
+            "Content-Type": "application/json",
+        },
+    )
+
+
+def tweet_new_and_cancelled_meetings(refresh_token):
+    # Update database
+    checker = TTCMeetingsChecker(
+        upcoming_url='https://www.ttc.ca//sxa/search/results/?customdaterangefacet=Upcoming',
+        past_url='https://www.ttc.ca//sxa/search/results/?s={5865C996-6A4C-472A-9116-C59CB3B76093}&itemid={1450DB42-0543-4C73-B159-421DF22D9460}&sig=past&p=8&o=ContentDateFacet%2CDescending&v=%7BF9A088B4-AFC4-4EE7-8649-0ACA83AB2783%7D'
+    )
+    posted_meetings = checker.get_upcoming_meetings()
+    known_meetings = checker.get_seen_meetings()
+    new, old, cancelled, completed = checker.get_diff_meetings(posted_meetings, known_meetings)
+    checker.update_database(new, cancelled, completed)
+
+    # Tweet
+    payload = {"text": f"Found {len(new)} meetings!"}
+    post_tweet(payload, refresh_token)
+    return
+
+
+@app.route("/run", methods=["GET"])
+def run():
+    # auth_client = OAuth2Session(client_id, client=oauthlib.oauth2.BackendApplicationClient(client_id), redirect_uri=redirect_uri, scope=scopes)
+    # Load OAuth token from redis
+    t = db.get("token")
+    bb_t = t.decode("utf8").replace("'", '"')
+    data = json.loads(bb_t)
+
+    token_url = "https://api.twitter.com/2/oauth2/token"
+
+    # Refresh token lasts 6 months
+    # refreshed_token = auth_client.refresh_token(
+    #     client_id=client_id,
+    #     client_secret=client_secret,
+    #     token_url=token_url,
+    #     refresh_token=data["refresh_token"],
+    #     # grant_type='client_credentials',
+    #     # access_token=data['access_token'],
+    #     headers={
+    #         "Authorization": "Basic RmKADOeccwyXwTI1rH9WTFoLH:NPQsLLyj72NSOEmNJt6uQ0m9b4rPx7ZfAiNUFYZ87IV54zVdi9",
+    #         "Content-Type": "application/x-www-form-urlencoded",
+    #     }
+    # )
+    refreshed_token = requests.post(
+        token_url,
+        data={
+            'client_id': client_id,
+            'refresh_token': data['refresh_token'],
+            'grant_type': 'refresh_token',
+        },
+        headers={
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization': f'Bearer {data["access_token"]}'
+        },
+    )
+    breakpoint()
+    if not refreshed_token.ok:
+        pass  # TODO
+    refreshed_token = refreshed_token.text
+    # Store in redis
+    st_refreshed_token = '"{}"'.format(refreshed_token)
+    j_refreshed_token = json.loads(st_refreshed_token)
+    db.set("token", refreshed_token)
+
+    # extra = {
+    #     'client_id': client_id,
+    #     'client_secret': client_secret
+    # }
+    # client = OAuth2Session(
+    #     client_id, token=data, auto_refresh_url=token_url,
+    #     auto_refresh_kwargs=extra, token_updater=save_token
+    # )
+    # refresh_token = client.get()
+
+    tweet_new_and_cancelled_meetings(refreshed_token)
 
 
 if __name__ == '__main__':
